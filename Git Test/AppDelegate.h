c87dd5a6ce0a4fa4f604887a2ef2d3773490df60
Dens <denis.roks@gmail.com>
2015-01-25 17:16:57 +0200

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Dens on 25.01.15.
+//  Copyright (c) 2015 Kate Babadzhanova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Dens on 25.01.15.
+//  Copyright (c) 2015 Kate Babadzhanova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
