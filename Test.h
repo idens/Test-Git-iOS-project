e2119f16ff314b8f721d9aa6173df0efbfb9fa68
idens <denis.roks@gmail.com>
2015-01-25 17:53:08 +0200

    added a test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Dens on 25.01.15.
+//  Copyright (c) 2015 Kate Babadzhanova. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Dens on 25.01.15.
+//  Copyright (c) 2015 Kate Babadzhanova. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
